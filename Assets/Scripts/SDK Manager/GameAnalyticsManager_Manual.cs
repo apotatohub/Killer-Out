7a35bb0ddd6c56e532c3fb132d3811f7d2169251
hrud09 <65780684+hrud09@users.noreply.github.com>
2021-09-06 12:33:21 +0600

    Game Analytics Added

---
+++ Assets/Scripts/SDK Manager/GameAnalyticsManager_Manual.cs
@@ -0,0 +1,64 @@
+﻿using GameAnalyticsSDK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameAnalitycsManager_Manual : MonoBehaviour, IGameAnalyticsATTListener
+{
+    public static GameAnalitycsManager_Manual Instance;
+
+    private void Awake()
+    {
+        if (Instance==null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            GameAnalytics.RequestTrackingAuthorization(this);
+        }
+        else
+        {
+            GameAnalytics.Initialize();                                                                              
+        }
+    }
+
+    public void GameAnalyticsATTListenerNotDetermined()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerRestricted()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerDenied()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerAuthorized()
+    {
+        GameAnalytics.Initialize();
+    }
+
+    public void OnGameStart(string levelNumber)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, "Level_Num", levelNumber);
+    }
+    public void OnGameSucceed(string levelNumber)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "Level_Num", levelNumber);
+    }
+    public void OnGameFailed(string levelNumber)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Fail, "Level_Num", levelNumber);
+    }
+    public void OnGameEnd(string levelNumber)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, "Level_Num", levelNumber);
+    }
+}
